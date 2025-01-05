e5a3f1653d568e2976b3fc8c617346669a94ad2a
chaphu <bigrockbam@gmail.com>
2025-01-05 22:28:12 +0700

    add : optimize 101

--- src/app.py
+++ src/app.py
@@ -1,84 +1,81 @@
-# from fastapi import FastAPI
-# from fastapi.responses import JSONResponse
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-# from services.chatbot import Chatbot
-# from adaptors.qdrant_adaptors import QdrantAdaptor
-
-# app = FastAPI()
-
-# origins = [
-#     "*",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# qdrant_adaptor = QdrantAdaptor()
-# qrant_client = qdrant_adaptor.client
-# chatbot = Chatbot(qrant_client)
-
-
-# class QueryRequest(BaseModel):
-#     user_query: str
-
-
-# @app.get("/", response_class=JSONResponse)
-# def health_check():
-#     try:
-#         if not qrant_client:
-#             return JSONResponse(status_code=500, content={"status": "unhealthy"})
-#         return {"status": "healthy"}
-#     except Exception as e:
-#         return JSONResponse(
-#             status_code=500, content={"status": "unhealthy", "error": str(e)}
-#         )
-
-
-# @app.post("/chatbot", response_class=JSONResponse)
-# def submit(request: QueryRequest):
-#     user_query = request.user_query
-#     response_message, reference_content = chatbot.response(user_query)
-#     return {
-#         "user_query": user_query,
-#         "result": {
-#             "response_message": response_message,
-#             "reference_content": reference_content,
-#         },
-#     }
-
 from fastapi import FastAPI, WebSocket
-from fastapi.websockets import WebSocketState
 from adaptors.qdrant_adaptors import QdrantAdaptor
 from services.chatbot import Chatbot
 from starlette.websockets import WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
+# Create a FastAPI instance
 app = FastAPI()
 qdrant_adaptor = QdrantAdaptor("medical")
 qrant_client = qdrant_adaptor.client
 chatbot = Chatbot(qrant_client)
 
+# Add CORS middleware to allow cross-origin requests
+origins = [
+    "*",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Define the WebSocket endpoint for chatbot connections
+class ConnectionManager:
+    """Class defining socket events for managing WebSocket connections."""
+
+    def __init__(self):
+        """Initialize the manager with an empty list of active connections."""
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        """Handle a new connection and accept the WebSocket.
+        Args:
+            websocket (WebSocket): The WebSocket connection to accept.
+        """
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        print(f"New connection. Total connections: {len(self.active_connections)}")
+
+    def disconnect(self, websocket: WebSocket):
+        """Handle a disconnection by removing the WebSocket from the list.
+        Args:
+            websocket (WebSocket): The WebSocket connection to disconnect.
+        """
+        self.active_connections.remove(websocket)
+        print(f"Connection closed. Total connections: {len(self.active_connections)}")
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        """Send a personal message to a specific WebSocket connection.
+        Args:
+            message (str): The message to send.
+            websocket (WebSocket): The WebSocket connection to send the message to.
+        """
+        await websocket.send_text(message)
+
+
+# Create a ConnectionManager instance to manage WebSocket connections
+manager = ConnectionManager()
+
+
+# Define the WebSocket endpoint for chatbot connections
 @app.websocket("/chatbot")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    """Handle WebSocket connections at this endpoint."""
+    await manager.connect(websocket)
     try:
         while True:
             user_message = await websocket.receive_text()
+            print(f"Received message: {user_message}")
             async for response in chatbot.stream_response(user_message):
-                await websocket.send_text(response)
+                await manager.send_personal_message(response, websocket)
     except WebSocketDisconnect:
+        manager.disconnect(websocket)
         print("Client disconnected")
     except Exception as e:
         print(f"Error: {e}")
-    finally:
-        try:
-            if websocket.client_state == WebSocketState.CONNECTED:
-                await websocket.close()
-        except Exception as e:
-            print(f"Error during WebSocket close: {e}")
+        manager.disconnect(websocket)
